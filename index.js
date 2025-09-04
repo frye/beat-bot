25cb83db7682c04eb5c3c4cf334a92299df0ca32
Samuli Jarvinen <frye@github.com>
2025-09-03 21:39:48 -0500

    Initialize testing

---
+++ index.js
@@ -0,0 +1,20 @@
+const app = require("express")(),
+      pg = require("pg"),
+      pool = new pg.Pool(config),
+      rateLimit = require("express-rate-limit");
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100 // limit each IP to 100 requests per windowMs
+});
+
+app.use("/search", limiter);
+
+app.get("search", function handler(req, res) {
+  // BAD: the category might have SQL special characters in it
+  var query1 =
+    "SELECT ITEM,FROM TABLE WHERE ITEM_CATEGORY=$1";
+  pool.query(query1, [req.params.category], function(err, results) {
+    // process results
+  });
+});
